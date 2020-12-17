01f0c5b5497861af4c0fb300c198ffd1945eca54
Jason Lenoble <jason.lenoble@gmail.com>
2020-12-17 16:59:27 +0100

    ♻️ refactor(commitlint-config): convert to typescript

---
+++ packages/commitlint-config/lib/index.d.ts
@@ -0,0 +1,6 @@
+import { RulesConfig } from "@commitlint/types";
+declare const config: {
+    parserPreset: string;
+    rules: RulesConfig<import("@commitlint/types").RuleConfigQuality.User>;
+};
+export default config;
