d5bfbda89e078041001c8bdebaef76b5356c5764
Jason Lenoble <jason.lenoble@gmail.com>
2021-04-28 18:18:42 +0200

    👷 build: clean branch to evaluate RushStack

--- packages/commitlint-config/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { RulesConfig } from "@commitlint/types";
-declare const config: {
-    parserPreset: string;
-    rules: RulesConfig<import("@commitlint/types").RuleConfigQuality.User>;
-};
-export default config;
--- packages/gulpfile/gulpfile-bootstrap.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const { spawn } = require("child_process");
-
-// If the cjs dir has been deleted, regenerate it for gulp to work properly
-// with command: gulp --gulpfile gulpfile-bootstrap.js
-// (or equivalently: npx tsc --project tsconfig-cjs.json)
-gulp.task("default", () =>
-  spawn("npx", ["tsc", "--project", "tsconfig-cjs.json"])
-);
