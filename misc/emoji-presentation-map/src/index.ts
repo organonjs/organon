4591075d8d2e9460e488b82039b7e1969be50598
Jason Lenoble <jason.lenoble@gmail.com>
2021-04-30 16:59:36 +0200

    ✨ feat(emoji-presentation-map):  add project to repo
    from packages/emojis in main branch

---
+++ misc/emoji-presentation-map/src/index.ts
@@ -0,0 +1,15 @@
+// emoji code points
+// keys: presentation is default (may be text)
+// values: presentation is emoji
+export const emojiPresentationMap = new Map([
+  ["👷", "\ud83d\udc77"],
+  ["💚", "\ud83d\udc9a"],
+  ["📝", "\ud83d\udcdd"],
+  ["✨", "\u2728"],
+  ["🐛", "\ud83d\udc1b"],
+  ["⚡", "\u26A1"],
+  ["♻", "\u267b\ufe0f"], // Force Emoji Presentation "♻️", see https://www.unicode.org/emoji/charts-13.1/text-style.html, row "2002 ⊖ Dings"
+  ["⏪", "\u23ea"],
+  ["🎨", "\ud83c\udfa8"],
+  ["✅", "\u2705"],
+]);
