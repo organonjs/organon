ad1ba97f65b13b8b10d054b9a85cdf742d70c060
Jason Lenoble <jason.lenoble@gmail.com>
2022-06-19 11:45:55 +0200

    ✅ test(conventional-changelog-organon): add test
    we check that the pattern emojis are 1/1 to the emoji presentation map
    entries without using an unsafe dynamic regexp.

--- tooling/conventional-changelog/src/__tests__/index.test.ts
+++ tooling/conventional-changelog/src/__tests__/index.test.ts
@@ -1,8 +1,12 @@
 import config from "@organon/commitlint-config";
+import { emojiPresentationMap } from "@organon/emoji-presentation-map";
 import { headerPattern } from "..";
 
 describe("commitlint config", () => {
   const tuple = config.rules["type-enum"];
+
+  // Make sure something bad happens if no tests are set
+  expect(tuple).toBeInstanceOf(Array);
 
   if (Array.isArray(tuple)) {
     // Type guard makes typescript happy
@@ -32,6 +36,18 @@ describe("commitlint config", () => {
 
         expect(headerPattern.test(commitHeader)).toBe(false);
       });
+    });
+
+    it(`header pattern exactly checks for all emojis in the emoji presentation map`, () => {
+      const emojis = Array.from(emojiPresentationMap.values()).join("");
+
+      let truncatedSource: string = headerPattern.source.substring(10);
+      truncatedSource = truncatedSource.substring(0, truncatedSource.indexOf("]"));
+      truncatedSource = truncatedSource.replace(/.\\ufe0f/g, (match: string): string => {
+        return emojiPresentationMap.get(match[0]) || "";
+      });
+
+      expect(emojis).toBe(truncatedSource);
     });
   }
 });
