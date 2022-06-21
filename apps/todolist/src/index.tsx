2cc333dc3bae826dcf6b34a7da36d10bbcc1eecd
Jason Lenoble <jason.lenoble@gmail.com>
2022-06-21 17:15:23 +0200

    ♻️ refactor(todolist): use react18 syntax
    react18 uses createRoot, root.render instead of ReactDom.render.
    
    This gets rid of a warning (likely an error in future react19).

--- apps/todolist/src/index.tsx
+++ apps/todolist/src/index.tsx
@@ -1,17 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot, Root } from "react-dom/client";
 import { BaseList, BaseListItem } from "./packlets/mui-base-list";
 import ListItemText from "@mui/material/ListItemText";
 
 const rootDiv: HTMLElement = document.getElementById("root") as HTMLElement;
-ReactDOM.render(
+const root: Root = createRoot(rootDiv);
+
+root.render(
   <BaseList>
     {["foo", "bar", "qux"].map((txt, i) => (
       <BaseListItem key={i}>
         <ListItemText primary={txt} secondary={"item" + i} />
       </BaseListItem>
     ))}
-  </BaseList>,
-  rootDiv
+  </BaseList>
 );
