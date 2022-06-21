2023b6db72a636b2479b2beee4567cbc56c38c5d
Jason Lenoble <jason.lenoble@gmail.com>
2022-06-21 18:00:07 +0200

    ✨ feat(todolist): add dnd
    Drag and Drop capability to list, but no persistence yet.

--- apps/todolist/src/index.tsx
+++ apps/todolist/src/index.tsx
@@ -1,18 +1,34 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from "react";
 import { createRoot, Root } from "react-dom/client";
+import { DragDropContext, DropResult, Droppable, Draggable } from "react-beautiful-dnd";
 import { BaseList, BaseListItem } from "./packlets/mui-base-list";
 import ListItemText from "@mui/material/ListItemText";
 
 const rootDiv: HTMLElement = document.getElementById("root") as HTMLElement;
 const root: Root = createRoot(rootDiv);
 
+const onDragEnd = (result: DropResult): void => {
+  console.log(result);
+};
+
 root.render(
-  <BaseList>
-    {["foo", "bar", "qux"].map((txt, i) => (
-      <BaseListItem key={i}>
-        <ListItemText primary={txt} secondary={"item" + i} />
-      </BaseListItem>
-    ))}
-  </BaseList>
+  <DragDropContext onDragEnd={onDragEnd}>
+    <Droppable droppableId="id">
+      {({ innerRef, droppableProps, placeholder }) => (
+        <BaseList ref={innerRef} {...droppableProps}>
+          {["foo", "bar", "qux"].map((txt, index) => (
+            <Draggable draggableId={"id" + index} index={index} key={index}>
+              {({ draggableProps, dragHandleProps, innerRef }) => (
+                <BaseListItem ref={innerRef} key={index} {...draggableProps} {...dragHandleProps}>
+                  <ListItemText primary={txt} secondary={"item" + index} />
+                </BaseListItem>
+              )}
+            </Draggable>
+          ))}
+          {placeholder}
+        </BaseList>
+      )}
+    </Droppable>
+  </DragDropContext>
 );
