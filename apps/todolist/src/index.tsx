ccea1685ffd751a6094b881d8b700550aa55fb2c
Jason Lenoble <jason.lenoble@gmail.com>
2022-06-21 03:32:07 +0200

    ✨ feat(todolist): first commit
    Mostly boiler plate to develop the react app.

---
+++ apps/todolist/src/index.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const rootDiv: HTMLElement = document.getElementById("root") as HTMLElement;
+ReactDOM.render(<span>Hello World!</span>, rootDiv);
