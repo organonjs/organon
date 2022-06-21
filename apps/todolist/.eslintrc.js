43ea7d98ab53fa9c37ab8b2150b9e72430fd1714
Jason Lenoble <jason.lenoble@gmail.com>
2022-06-21 14:33:33 +0200

    👷 build(eslint): enable packlets

--- apps/todolist/.eslintrc.js
+++ apps/todolist/.eslintrc.js
@@ -2,6 +2,10 @@
 require("@rushstack/eslint-config/patch/modern-module-resolution");
 
 module.exports = {
-  extends: ["@rushstack/eslint-config/profile/web-app", "@rushstack/eslint-config/mixins/react"],
+  extends: [
+    "@rushstack/eslint-config/profile/web-app",
+    "@rushstack/eslint-config/mixins/react",
+    "@rushstack/eslint-config/mixins/packlets",
+  ],
   parserOptions: { tsconfigRootDir: __dirname },
 };
